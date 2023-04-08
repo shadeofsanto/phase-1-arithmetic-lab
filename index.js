15ac097e96c39bdb0f7a226a60b0a1e3ba5da05f
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-04-08 14:15:19 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 2
+let num4 = 8
+
+let mod = num4 / num3
+
+let max = Math.max(1,5,20)
